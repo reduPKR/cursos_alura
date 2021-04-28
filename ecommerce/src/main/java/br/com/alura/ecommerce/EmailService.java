6f4f675381e7d670a9dd949f85a625060d291a1b
redu <reduardo19942@gmail.com>
2021-04-28 15:30:13 -0300

    Curso kafka - consumer envio de email e logs

---
+++ ecommerce/src/main/java/br/com/alura/ecommerce/EmailService.java
@@ -0,0 +1,48 @@
+package br.com.alura.ecommerce;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+public class EmailService {
+    public static void main(String[] args) {
+        var consumer = new KafkaConsumer<String, String>(properties());
+        consumer.subscribe(Collections.singletonList("ECOMMERCE_SEND_EMAIL"));
+
+        while (true){
+            var records = consumer.poll(Duration.ofMillis(100));
+            if (!records.isEmpty()) {
+                System.out.println("Novo registro encontrado");
+
+                for (var item : records) {
+                    System.out.println("***********************************");
+                    System.out.println("Enviando email");
+                    System.out.println(item.key());
+                    System.out.println(item.value());
+                    System.out.println(item.partition());
+                    System.out.println(item.offset());
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println("Email enviado");
+                }
+            }
+        }
+    }
+
+    private static Properties properties() {
+        var properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,"127.0.0.1:9092");
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, EmailService.class.getSimpleName());
+
+        return properties;
+    }
+}
--- ecommerce/src/main/java/br/com/alura/ecommerce/NewOrderMain.java
+++ ecommerce/src/main/java/br/com/alura/ecommerce/NewOrderMain.java
@@ -1,5 +1,6 @@
 package br.com.alura.ecommerce;
 
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -13,20 +14,31 @@ public class NewOrderMain {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
                          //KafkaProducer<Key, Value> -> do metodo properties
         var producer = new KafkaProducer<String,String>(properties());
-        String value = "1234,4567,7890";//Por ser teste vai representar a chave e o valor
+
+        String value = "12345,67890,12345";//Por ser teste vai representar a chave e o valor
+        String email = "Estamos processando seu pedido";
 
         //var record = new ProducerRecord<String, String>("ECOMMERCE_NEW_ORDER",value,value);
         var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER",value,value);//Ele idendifica que sao duas strings por isso n escrevi
+        var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL",email,email);
 
         //producer.send(record).get();
         //producer.send(record).get();//Com o get avisa que esta espereando a resposta
-        producer.send(record, (data, ex) ->{
-            if(ex != null){
+
+        Callback callback = getCallback();
+
+        producer.send(record, callback).get();
+        producer.send(emailRecord, callback).get();
+    }
+
+    private static Callback getCallback() {
+        return (data, ex) -> {
+            if (ex != null) {
                 ex.printStackTrace();
                 return;
             }
-            System.out.println("Sucesso\n"+data.topic()+"\n"+data.partition()+"\n"+data.offset()+"\n"+data.timestamp());
-        }).get();//Alem de esperar a resposta eu quero exibir o resultado.
+            System.out.println("Sucesso\n" + data.topic() + "\n" + data.partition() + "\n" + data.offset() + "\n" + data.timestamp());
+        };
     }
 
     private static Properties properties() {
